15c0781f6b9bf4b13f7160b7814f2cb6b7d6b82a
蒋宇轩 <1312534629@qq.com>
2025-02-10 23:26:35 +0800

    Site updated: 2025-02-10 23:26:17

--- js/zhihu.js
+++ js/zhihu.js
@@ -1,12 +1,12 @@
-fetch('https://tenapi.cn/v2/zhihuhot?format=json').then(data=>data.json()).then((json)=>{
+fetch('https://api.cenguigui.cn/api/juhe/hotlist.php?type=zhihu').then(data=>data.json()).then((json)=>{
     let html = ''
     html += '<div class="zhihu-list">'
     var i = 1 
     for (let item of json.data) {
-        html += '<div class="zhihu-list-item"><div class="zhihu-hotness">' + i + '</div>' + '<span class="zhihu-title"><a title="' + item.title + '"href="' + item.url + '" target="_blank" rel="external nofollow noreferrer">' + item.title + '</a></span>' + '<div class="zhihu-hot"><span>' + item.hot + '</span></div></div>'
+        html += '<div class="zhihu-list-item"><div class="zhihu-hotness">' + i + '</div>' + '<span class="zhihu-title"><a title="' + item.title + '"href="' + item.url + '" target="_blank" rel="external nofollow noreferrer">' + item.title + '</a>热搜1</span>' + '<div class="zhihu-hot"><span>' + item.hot + '</span></div></div>'
         i++
     }
-    html += '热搜1</div>'
+    html += '</div>'
     document.getElementById('zhihu-container').innerHTML = html
 }).catch(function(error) {
     console.log(error);
